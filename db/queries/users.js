472e416a346d33861ecabae49b66aa3d5354161d
Andrew Roth <a.roth.80@hotmail.com>
2025-05-10 01:46:33 -0700

    2 queries added get listing and get user

--- db/queries/users.js
+++ db/queries/users.js
@@ -1,10 +1,23 @@
 const db = require('../connection');
 
-const getUsers = () => {
-  return db.query('SELECT * FROM users;')
-    .then(data => {
-      return data.rows;
-    });
+const getUsersWithListings = () => {
+  return db.query(`
+    SELECT
+      Users.UserID,
+      Users.FirstName,
+      Users.LastName,
+      Users.Email,
+      Listings.ListingID,
+      Listings.Price,
+      Listings.DatePosted,
+      Listings.Description
+    FROM Users
+    LEFT JOIN Listings ON Users.UserID = Listings.UserID
+    ORDER BY Users.UserID, Listings.ListingID;
+  `)
+  .then(data => {
+    return data.rows;
+  });
 };
 
-module.exports = { getUsers };
+module.exports = { getUsersWithListings };
