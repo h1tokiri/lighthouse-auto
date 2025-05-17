310f82c4f5b0493a19d91aff638a602425e28bbd
Javin Leslie Lau <javinlau21@gmail.com>
2025-05-16 22:40:42 -0400

    fixed merges between three musketeers

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,17 +1,39 @@
+// import React from "react";
+// import "./index.css";
+// import { Routes, Route } from "react-router-dom";
+// import * as UI from "./components/ui";
+// import DetailedListingsPage from "./pages/DetailedListingsPage";
+
+// const App = () => {
+//   return (
+//     <Router>
+//       <Routes>
+//         <Route path="/" element={<HomePage />} />
+//       </Routes>
+//     </Router>
+//   );
+// };
+
+// export default App;
+
 import React from "react";
 import "./index.css";
 import { Routes, Route } from "react-router-dom";
 import * as UI from "./components/ui";
-import DetailedListingsPage from './pages/DetailedListingsPage';
+import DetailedListingsPage from "./pages/DetailedListingsPage";
 
-const App = () => {
+export default function App() {
   return (
-    <Router>
-      <Routes>
-        <Route path="/" element={<HomePage />} />
-      </Routes>
-    </Router>
+    <div className="app-root">
+      <header className="app-header">
+        <h1 className="app-title">🚗 Welcome to Lighthouse Auto!</h1>
+        <UI.Button>Click me</UI.Button>
+      </header>
+      <main className="app-main">
+        <Routes>
+          <Route path="/vehicles/:id" element={<DetailedListingsPage />} />
+        </Routes>
+      </main>
+    </div>
   );
-};
-
-export default App;
+}
--- client/vite.config.js
+++ client/vite.config.js
@@ -12,7 +12,7 @@ export default defineConfig({
   plugins: [react()],
   server: {
     proxy: {
-      "/api": "http://localhost:8080",
+      "/api": "http://localhost:3001",
     },
   },
   resolve: {
