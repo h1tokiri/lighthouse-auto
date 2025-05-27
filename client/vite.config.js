7aba5a87b6ba15fa5767b99143de38bafe12e6b8
Javin Leslie Lau <javinlau21@gmail.com>
2025-05-27 11:47:37 +0800

    updated vite

--- client/vite.config.js
+++ client/vite.config.js
@@ -1,18 +1,21 @@
 // vite.config.js
-import { defineConfig } from "vite";
+import { defineConfig, loadEnv } from "vite";
 import react from "@vitejs/plugin-react";
 
-export default defineConfig({
-  plugins: [react()],
-  server: {
-    proxy: {
-      "/api": import.meta.env.VITE_API_URL ? import.meta.env.VITE_API_URL : "http://localhost:3001",
-      "/uploads": {
-        target: import.meta.env.VITE_API_URL
-          ? import.meta.env.VITE_API_URL
-          : "http://localhost:3001",
-        changeOrigin: true,
+export default ({ mode }) => {
+  // Load app-level env vars to node-level env vars.
+  process.env = { ...process.env, ...loadEnv(mode, process.cwd()) };
+
+  return defineConfig({
+    plugins: [react()],
+    server: {
+      proxy: {
+        "/api": process.env.VITE_API_URL ? process.env.VITE_API_URL : "http://localhost:3001",
+        "/uploads": {
+          target: process.env.VITE_API_URL ? process.env.VITE_API_URL : "http://localhost:3001",
+          changeOrigin: true,
+        },
       },
     },
-  },
-});
+  });
+};
